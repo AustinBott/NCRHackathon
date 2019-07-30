6f235f3953b17e8483cf55212786a0a433840ac2
andrew.scheinbach <andrew.scheinbach@tinroofsoftware.com>
2019-07-30 16:42:06 -0400

    OOPS

---
+++ MonitoringUI/sensuconfigurationtool/src/components/component-items/agentCard.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <table border="4">
+            <tr>
+                <td>{{agent.name}}</td>
+                <td>{{agent.namespace}}</td>
+            </tr>
+            <tr>
+                <td>{{agent.lastUpdate}}</td>
+                <td>{{agent.label}}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props:{
+        agent:{
+            name : String,
+            namespace : String,
+            lastUpdate : String,
+            label : String
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
