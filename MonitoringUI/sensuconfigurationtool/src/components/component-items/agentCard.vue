6e5e63b6c7a8f9b27c20de18e7163833542766b0
andrew.scheinbach <andrew.scheinbach@tinroofsoftware.com>
2019-07-30 17:50:34 -0400

    The 'Its not awful ' css

--- MonitoringUI/sensuconfigurationtool/src/components/component-items/agentCard.vue
+++ MonitoringUI/sensuconfigurationtool/src/components/component-items/agentCard.vue
@@ -1,15 +1,15 @@
 <template>
-    <div>
-        <table border="4">
-            <tr>
-                <td>{{agent.name}}</td>
-                <td>{{agent.namespace}}</td>
-            </tr>
-            <tr>
-                <td>{{agent.lastUpdate}}</td>
-                <td>{{agent.label}}</td>
-            </tr>
-        </table>
+    <div class = "card">
+        <div class = "table">
+            <div class = "row">
+                <div class = "item">{{agent.name}}</div>
+                <div class = "item">{{agent.namespace}}</div>
+            </div>
+            <div class="row">
+                <div class = "item">{{agent.lastUpdate}}</div>
+                <div class = "item" >{{agent.label}}</div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -27,6 +27,32 @@ export default {
 }
 </script>
 
-<style>
-
-</style>
+<style scoped>
+    .card{
+        width: 100%;
+        display: flex;
+        border: 1px blue solid;
+    }
+    .table{
+        width: 100%;
+        border: 1px green solid;
+        display: flex;
+        justify-content: space-evenly;
+        flex-direction: column;
+        overflow: hidden;
+    }
+    .row{
+        border: 1px red solid;
+        display: flex;
+        flex-direction: row;
+        margin: 2px;
+        overflow: hidden;
+        justify-content: space-evenly;
+    }
+    .item{
+        margin: 5px;
+        padding: 5px;
+        font: 2px;
+        overflow: hidden;
+    }
+    </style>
