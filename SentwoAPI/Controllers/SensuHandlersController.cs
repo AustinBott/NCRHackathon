bb7390423330705002586a5f6c4e6cba8d3858ea
QL185017 <Qianzhen.Li@ncr.com>
2019-07-31 14:04:56 -0400

    add get method to handler controller

--- SentwoAPI/Controllers/SensuHandlersController.cs
+++ SentwoAPI/Controllers/SensuHandlersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RestSharp;
 
 namespace SentwoAPI.Controllers
 {
@@ -11,5 +12,30 @@ namespace SentwoAPI.Controllers
     [ApiController]
     public class SensuHandlersController : ControllerBase
     {
+        private readonly string url = "https://sensuservice.preprod.ncrsaas.com/api/core/v2/namespaces/cso2_development/handlers";
+
+        [HttpGet()]
+        public ActionResult<object> Get()
+        {
+            string authToken = new SensuAuthenticationController().GetAuthenticationToken();
+            var client = new RestClient(url);
+            client.AddDefaultHeader("Authorization", "Bearer " + authToken);
+            var request = new RestRequest("", Method.GET);
+            var response = client.Execute(request);
+            return this.Content(response.Content, "application/json");
+        }
+
+        [HttpGet("{handlerName}")]
+        public ActionResult<object> Get(string handlerName)
+        {
+            string authToken = new SensuAuthenticationController().GetAuthenticationToken();
+            var client = new RestClient(url);
+            client.AddDefaultHeader("Authorization", "Bearer " + authToken);
+            var request = new RestRequest(handlerName, Method.GET);
+            var response = client.Execute(request);
+            return this.Content(response.Content, "application/json");
+        }
+
+
     }
 }
