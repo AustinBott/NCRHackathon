91da173087c5a200dc69e7653bc3216e8ab040e4
sebHollister <seb.hollister@gatech.edu>
2019-07-31 10:28:17 -0400

    removed sensuController

---
+++ SentwoAPI/Controllers/SensuController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SentwoAPI.Models;
+using Nest;
+using System.Net.Http;
+
+namespace SentwoAPI.Controllers
+{
+    
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SensuController : ControllerBase
+    {
+
+        private static readonly HttpClient client = new HttpClient();
+
+        public SensuController()
+        {
+            //TODO do authentication
+        }
+
+        [HttpPost]
+        public void PostEntrySensu(Entry entry)
+        {
+
+
+        }
+    }
+}
